e8ecc2b3bbd0f59c090b912e269a99c386af4b89
Cherichenko_Sl <Cherry-slava@yandex.ru>
2022-02-10 18:54:51 +0300

    Task4 finished

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿static int GetFibonacci(int number)
+{
+    if (number == 0 || number == 1)
+    {
+        return number;
+    }
+    return GetFibonacci(number - 1) + GetFibonacci(number - 2);
+}
+int number = 7;
+Console.WriteLine(GetFibonacci(number));
