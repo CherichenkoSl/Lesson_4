928e90d62a2d9717a83242f5385a009ceab0a4c4
Cherichenko_Sl <Cherry-slava@yandex.ru>
2022-02-10 15:49:15 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿static string GetFullName(string firstName, string lastName, string patronymic)
+{
+    string FullName = $"{lastName} {firstName} {patronymic}";
+    return FullName;
+}
+Console.WriteLine($"ФИО пользователя: {GetFullName("Владислав", "Суворов", "Валерьевич")}");
+Console.WriteLine($"ФИО пользователя: {GetFullName("Вячеслав", "Чериченко", "Владимирович")}");
+Console.WriteLine($"ФИО пользователя: {GetFullName("Иван","Иванов","Иванович")}");
+Console.WriteLine($"ФИО пользователя: {GetFullName("Дмитрий", "Тимофеев", "Юрьевич")}");
